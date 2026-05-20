e396999acdc1a0990864f8f06fe5eeab896c00f5
Dmytro Holub <heap3d@gmail.com>
2026-05-20 10:24:27 +0300

    added: complex polygons detection complex edges detection edge loops detection (slow)

--- scripts/complex_polygons_tools.py
+++ scripts/complex_polygons_tools.py
@@ -8,166 +8,222 @@
 # utilites for complex polygons detection and processing
 # ================================
 
-from typing import Iterable, Optional, Union
+from typing import Iterable, Union
 from dataclasses import dataclass
 
 import lx
-import modo
+from modo import Scene, Item, Mesh, MeshPolygon, MeshEdge, MeshVertex, Vector3
 import modo.constants as c
 
 from h3d_utilites.scripts.h3d_utils import SELECTION_MODE, set_selection_mode, drop_selection
 
+from h3d_utilites.scripts.h3d_debug import (
+    execution_time,
+    # fn_in,
+    # fn_out,
+    # prints,
+    )
 
-POLYGON = SELECTION_MODE.POLYGON.value
+
 ITEM = SELECTION_MODE.ITEM.value
+POLYGON = SELECTION_MODE.POLYGON.value
 EDGE = SELECTION_MODE.EDGE.value
 VERTEX = SELECTION_MODE.VERTEX.value
 
 
 @dataclass
-class ComplexPolygon:
-    polygon: modo.MeshPolygon
-    complex_edges: list[modo.MeshEdge]
-    mesh: modo.Mesh
-
-
-@dataclass
-class VertexPair:
-    vertex1: modo.MeshVertex
-    vertex2: modo.MeshVertex
-
-
-@dataclass
 class BoundingBox:
-    min: modo.Vector3
-    max: modo.Vector3
+    corner1: Vector3
+    corner2: Vector3
 
 
-Component = Union[modo.MeshVertex, modo.MeshEdge, modo.MeshPolygon]
+Component = Union[MeshVertex, MeshEdge, MeshPolygon]
 
 
-def get_complex_polygons(mesh: modo.Mesh) -> list[ComplexPolygon]:
-    if not mesh:
-        raise ValueError('Mesh is None')
-
+@execution_time
+def get_complex_geometry(mesh: Mesh) -> tuple[list[MeshPolygon], list[MeshEdge]]:
+    # fn_in()
     complex_edges = get_complex_edges(mesh)
 
-    complex_polygons = set()
+    complex_polygons: set[MeshPolygon] = set()
     for edge in complex_edges:
         polygons = edge.polygons
         if not polygons:
             continue
         complex_polygons.update(polygons)
 
-    complex_polygons_data = []
-    for polygon in complex_polygons:
-        complex_polygon = ComplexPolygon(
-            polygon=polygon,
-            complex_edges=[edge for edge in complex_edges if polygon in edge.polygons],
-            mesh=mesh
-        )
-        complex_polygons_data.append(complex_polygon)
-
-    return complex_polygons_data
+    # fn_out()
+    return list(complex_polygons), list(complex_edges)
 
 
-def get_complex_edges(mesh: modo.Mesh) -> list[modo.MeshEdge]:
-    if not mesh:
-        raise ValueError('Mesh is None')
+# @execution_time
+def get_complex_edges(mesh: Mesh) -> list[MeshEdge]:
+    # fn_in()
+    geometry = mesh.geometry
+    if not geometry:
+        raise ValueError(f'Mesh <{mesh.name}> has no geometry')
+    polygons = geometry.polygons
+    if not polygons:
+        raise ValueError(f'Mesh <{mesh.name}> has no polygons')
 
-    one_polygon_edges = get_one_polygon_edges(mesh)
-    open_edges = get_open_edges(mesh)
+    one_polygon_edges = get_one_polygon_edges(polygons)
+    open_edges = get_open_edges_all(polygons)
     complex_edges = set(one_polygon_edges) - set(open_edges)
 
+    # fn_out()
     return list(complex_edges)
 
 
-def get_one_polygon_edges(mesh: modo.Mesh) -> list[modo.MeshEdge]:
+@execution_time
+def get_one_polygon_edges(polygons: Iterable[MeshPolygon]) -> list[MeshEdge]:
+    meshes = get_containing_meshes(polygons)
+    if not meshes:
+        raise ValueError('No meshes found for polygons')
 
-    if not mesh:
-        raise ValueError('Mesh is None')
+    selected_edges: set[MeshEdge] = set()
+    for mesh in meshes:
+        mesh.select(replace=True)
+        drop_selection(POLYGON)
+        drop_selection(EDGE)
 
-    mesh.select(replace=True)
-    lx.eval('select.edge add poly equal 1')
+        # select all 1-polygon edges
+        lx.eval('select.edge add poly equal 1')
 
-    geometry = mesh.geometry
-    if not geometry:
-        raise ValueError('Mesh has no geometry')
+        geometry = mesh.geometry
+        if not geometry:
+            raise ValueError(f'Mesh <{mesh.name}> has no geometry')
+        edges = geometry.edges
+        if not edges:
+            raise ValueError(f'Mesh <{mesh.name}> has no edges')
 
-    edges = geometry.edges
-    if not edges:
-        raise ValueError('Mesh has no edges')
+        selected_edges.update(edges.selected)
+        lx.eval('select.drop edge')
 
-    selected_edges = edges.selected
-    lx.eval('select.drop edge')
-
-    return selected_edges
-
-
-def get_open_edges(mesh: modo.Mesh) -> list[modo.MeshEdge]:
-    if not mesh:
-        raise ValueError('Mesh is None')
-
-    select_boundary_edges(mesh, None)
-
-    geometry = mesh.geometry
-    if not geometry:
-        raise ValueError('Mesh has no geometry')
-
-    edges = geometry.edges
-    if not edges:
-        raise ValueError('Mesh has no edges')
-
-    selected_edges = edges.selected
-    lx.eval('select.drop edge')
-
-    return selected_edges
+    return [edge for edge in selected_edges if is_edge_belong_to(edge, polygons)]
 
 
-def select_boundary_edges(mesh: modo.Mesh, polygons: Optional[Iterable[modo.MeshPolygon]]):
-    if not mesh:
-        raise ValueError('Mesh is None')
+@execution_time
+def get_containing_meshes(components: Iterable[Component]) -> list[Mesh]:
+    meshes: set[Mesh] = set()
+    for component in components:
+        geometry = component._geometry
+        if not geometry:
+            raise ValueError('Component has no geometry')
+        item = geometry._item
+        if not item:
+            raise ValueError('Geometry has no item')
+        id = item.Ident()
+        if id is None:
+            raise ValueError('Item has no id')
+        mesh = Mesh(id)
+        if not mesh:
+            raise ValueError(f'Mesh with id {id} not found')
+        meshes.add(mesh)
 
-    mesh.select(replace=True)
-    drop_selection(POLYGON)
-    if polygons:
+    return list(meshes)
+
+
+def is_edge_belong_to(edge: MeshEdge, polygons: Iterable[MeshPolygon]) -> bool:
+    for polygon in edge.polygons:
+        if polygon in polygons:
+            return True
+
+    return False
+
+
+@execution_time
+def get_open_edges_all(polygons: Iterable[MeshPolygon]) -> list[MeshEdge]:
+    meshes = get_containing_meshes(polygons)
+    if not meshes:
+        raise ValueError('No meshes found for polygons')
+
+    selected_edges: set[MeshEdge] = set()
+    for mesh in meshes:
+        mesh.select(replace=True)
+        drop_selection(POLYGON)
+        drop_selection(EDGE)
+
+        # Select boundary edges
+        lx.eval('script.run "macro.scriptservice:92663570022:macro"')
+
+        geometry = mesh.geometry
+        if not geometry:
+            raise ValueError(f'Mesh <{mesh.name}> has no geometry')
+        edges = geometry.edges
+        if not edges:
+            raise ValueError(f'Mesh <{mesh.name}> has no edges')
+
+        selected_edges.update(edges.selected)
+        drop_selection(EDGE)
+
+    return list(selected_edges)
+
+
+@execution_time
+def get_open_edges_specified(polygons: Iterable[MeshPolygon]) -> list[MeshEdge]:
+    meshes = get_containing_meshes(polygons)
+    if not meshes:
+        raise ValueError('No meshes found for polygons')
+
+    selected_edges: set[MeshEdge] = set()
+    for mesh in meshes:
+        mesh.select(replace=True)
+        drop_selection(POLYGON)
+        drop_selection(EDGE)
+
         for polygon in polygons:
             polygon.select()
 
-    # Select boundary edges
-    lx.eval('script.run "macro.scriptservice:92663570022:macro"')
+        # Select boundary edges
+        lx.eval('script.run "macro.scriptservice:92663570022:macro"')
+
+        geometry = mesh.geometry
+        if not geometry:
+            raise ValueError(f'Mesh <{mesh.name}> has no geometry')
+        edges = geometry.edges
+        if not edges:
+            raise ValueError(f'Mesh <{mesh.name}> has no edges')
+
+        selected_edges.update(edges.selected)
+        drop_selection(EDGE)
+
+    return list(selected_edges)
 
 
-def select_complex_components(complex_polygons: Iterable[ComplexPolygon]):
-    modo.Scene().deselect()
-    set_selection_mode(POLYGON)
-    drop_selection(POLYGON)
-
-    for complex_polygon in complex_polygons:
-        complex_polygon.polygon.select()
-        complex_polygon.mesh.select()
-        for edge in complex_polygon.complex_edges:
-            edge.select()
+@execution_time
+def select_components(components: Iterable[Component]):
+    for component in components:
+        component.select()
 
 
-def create_aligned_loc(mesh: modo.Mesh, component_type: str, components: Iterable[Component]) -> modo.Item:
+@execution_time
+def create_aligned_loc(component_type: str, components: Iterable[Component]) -> Item:
     LOCATOR_NAME = 'ComplexPolygon_Loc'
-
-    if not mesh:
-        raise ValueError('Mesh is None')
-
-    if not component_type:
-        raise ValueError('Component type is None')
-
-    if not components:
-        raise ValueError('Components is None')
 
     if not is_component_mode(component_type):
         raise ValueError('Component type must be vertex, edge or polygon')
 
-    mesh.select(replace=True)
+    drop_selection(POLYGON)
+    drop_selection(EDGE)
+    drop_selection(VERTEX)
+
+    components_by_type = [
+        component
+        for component in components
+        if is_component_of_type(component, component_type)
+        ]
+    if not components_by_type:
+        raise ValueError(f'No components of type {component_type} found in the list')
+
+    meshes = get_containing_meshes(components_by_type)
+    if not meshes:
+        raise ValueError('No meshes found for components')
+
+    drop_selection(ITEM)
+    for mesh in meshes:
+        mesh.select()
     set_selection_mode(component_type)
-    select_components(mesh, component_type, components)
+    select_components(components_by_type)
 
     lx.eval('workPlane.fitSelect')
     drop_selection(VERTEX)
@@ -175,7 +231,7 @@ def create_aligned_loc(mesh: modo.Mesh, component_type: str, components: Iterabl
     drop_selection(POLYGON)
 
     set_selection_mode(ITEM)
-    locator = modo.Scene().addItem(itype=c.LOCATOR_TYPE, name=LOCATOR_NAME)
+    locator = Scene().addItem(itype=c.LOCATOR_TYPE, name=LOCATOR_NAME)
 
     locator.select(replace=True)
     lx.eval('item.matchWorkplane pos')
@@ -186,45 +242,58 @@ def create_aligned_loc(mesh: modo.Mesh, component_type: str, components: Iterabl
     return locator
 
 
-def select_components(mesh: modo.Mesh, select_type: str, components: Iterable[Component], replace: bool = True):
-    if not mesh or not isinstance(mesh, modo.Mesh):
-        raise TypeError('Invalid mesh provided.')
-
-    if not mesh.geometry:
-        raise ValueError('Mesh has no geometry.')
-
-    lx.eval(f'select.type {select_type}')
-    if select_type not in (VERTEX, EDGE, POLYGON):
-        return
-
-    lx.eval(f'select.drop {select_type}')
-
-    if select_type == VERTEX:
-        if mesh.geometry.vertices:
-            mesh.geometry.vertices.select(vertices=components)
-    if select_type == EDGE:
-        if mesh.geometry.edges:
-            mesh.geometry.edges.select(edges=components)
-    if select_type == POLYGON:
-        if mesh.geometry.polygons:
-            mesh.geometry.polygons.select(polygons=components)
+# @execution_time
+def is_component_of_type(component: Component, component_type: str) -> bool:
+    if component_type == VERTEX:
+        return isinstance(component, MeshVertex)
+    elif component_type == EDGE:
+        return isinstance(component, MeshEdge)
+    elif component_type == POLYGON:
+        return isinstance(component, MeshPolygon)
+    else:
+        raise ValueError('Component type must be vertex, edge or polygon')
 
 
+# @execution_time
 def is_component_mode(select_type: str) -> bool:
     return select_type in (VERTEX, EDGE, POLYGON)
 
 
-def detect_edge_loops(complex_poly: ComplexPolygon) -> list[list[modo.MeshEdge]]:
+@execution_time
+def detect_edge_loops(edges: Iterable[MeshEdge]) -> list[list[MeshEdge]]:
+    """
+    detect edge loops from the list of edges. Edge loop is a sequence of connected edges.
+    """
+    # fn_in()
+    edges_to_process = set(edges)
+    edge_loops: list[list[MeshEdge]] = []
+    while edges_to_process:
+        edge = edges_to_process.pop()
+        connected_edges = get_connected_edges(edge, edges_to_process)
+        edge_loops.append([edge] + connected_edges)
+        edges_to_process -= set(connected_edges)
+
+    # fn_out()
+    return edge_loops
+
+
+# @execution_time
+def get_connected_edges(edge: MeshEdge, edges_to_test: Iterable[MeshEdge]) -> list[MeshEdge]:
+    testing_edges = set(edges_to_test) - {edge}
+    for test_edge in testing_edges:
+        if is_connected(test_edge, edge):
+            return [test_edge, ] + get_connected_edges(test_edge, testing_edges - {test_edge})
+
+    return []
+
+
+def is_connected(edge1: MeshEdge, edge2: MeshEdge) -> bool:
+    return any(vertex in edge2.vertices for vertex in edge1.vertices)
+
+
+def get_edges_bounding_box(edges: Iterable[MeshEdge], polygon: MeshPolygon) -> BoundingBox:
     ...
 
 
-def get_edges_bounding_box(edges: Iterable[modo.MeshEdge], complex_poly: ComplexPolygon) -> BoundingBox:
-    ...
-
-
-def is_intersect(vertex_pair: VertexPair, edges: Iterable[modo.MeshEdge], complex_poly: ComplexPolygon) -> bool:
-    ...
-
-
-def is_polygon_complex(polygon: modo.MeshPolygon) -> bool:
+def is_polygon_complex(polygon: MeshPolygon) -> bool:
     ...
--- scripts/detect_complex_polygons.py
+++ scripts/detect_complex_polygons.py
@@ -9,36 +9,61 @@
 import modo
 import modo.constants as c
 
+from h3d_utilites.scripts.h3d_utils import drop_selection, set_selection_mode, select_if_exists, execution_time_alarm
+
 from h3d_slice_complex_polygons.scripts.complex_polygons_tools import (
-    get_complex_polygons,
+    get_complex_geometry,
     detect_edge_loops,
     create_aligned_loc,
-    select_complex_components,
+    select_components,
+    get_containing_meshes,
+    get_open_edges_specified,
+    POLYGON,
     EDGE,
     )
 
+from h3d_utilites.scripts.h3d_debug import (
+    h3dd,
+    # execution_time,
+    # prints,
+    )
 
+
+@execution_time_alarm
 def main():
     meshes = modo.Scene().selectedByType(itype=c.MESH_TYPE)
     if not meshes:
         print('No meshes selected')
         return
 
-    complex_polygons_data = []
+    complex_polygons: list[modo.MeshPolygon] = []
+    complex_edges: list[modo.MeshEdge] = []
     for mesh in meshes:
-        complex_polygons_data.extend(get_complex_polygons(mesh))
+        polygons, edges = get_complex_geometry(mesh)
+        complex_polygons.extend(polygons)
+        complex_edges.extend(edges)
 
-    if not complex_polygons_data:
+    if not complex_polygons:
         print('No complex polygons found')
         return
 
-    for complex_polygon_data in complex_polygons_data:
-        edge_loops = detect_edge_loops(complex_polygon_data)
-        for edge_loop in edge_loops:
-            create_aligned_loc(complex_polygon_data.mesh, EDGE, edge_loop)
+    boundary_edges = get_open_edges_specified(complex_polygons)
 
-    select_complex_components(complex_polygons_data)
+    edge_loops = detect_edge_loops(boundary_edges)
+    for edge_loop in edge_loops:
+        create_aligned_loc(EDGE, edge_loop)
+
+    select_if_exists(get_containing_meshes(complex_polygons))
+
+    set_selection_mode(POLYGON)
+    drop_selection(POLYGON)
+    select_components(complex_polygons)
+
+    drop_selection(EDGE)
+    for edge_loop in edge_loops:
+        select_components(edge_loop)
 
 
 if __name__ == '__main__':
+    h3dd.enable_debug_output(True)
     main()
